ed6027b0c8e13a57ee9ca3abaf864e26c90aac20
jd114506 <jonathon.bramich@my.jcu.edu.au>
2025-07-13 21:18:17 +1000

    Add car.py to Prac_06

--- prac_06/car.py
+++ prac_06/car.py
@@ -1,7 +1,6 @@
 """CP1404/CP5632 Practical - Car class example."""
 class Car:
     """Represent a Car object."""
-
     def __init__(self, name = "", fuel=0):
         """Initialise a Car instance.
 
