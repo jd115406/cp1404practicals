bf0aa566302b87fbc0b87dc206f56f803091a2a6
jd114506 <jonathon.bramich@my.jcu.edu.au>
2025-07-20 19:53:25 +1000

    Add load_projects function to project_management

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -11,12 +11,12 @@ MENU =  "(L)oad projects\n" \
 FILENAME = 'projects.txt'
 
 def main():
-    project_list = []
     print(MENU)
     choice = input(">>>").upper()
     while choice != 'Q':
         if choice == 'L':
-            pass
+            load_projects()
+            print("Project loaded")
         elif choice == 'S':
             pass
         elif choice == 'D':
@@ -32,3 +32,19 @@ def main():
         print(MENU)
         choice = input(">>>").upper()
     print('Goodbye')
+
+def load_projects():
+    """Reads .txt file and return class attributes for each project"""
+    project_list = []
+    with open(FILENAME, 'r') as in_file:
+        in_file.readline()  # Skips header line
+        for line in in_file:
+            parts = line.strip().split('\t')
+            name = parts[0]
+            start_date = parts[1]
+            priority = int(parts[2])
+            cost_estimate = float(parts[3])
+            completion_percentage = float(parts[4])
+            project = Project(name, start_date, priority, cost_estimate, completion_percentage)
+            project_list.append(project)
+        return project_list
