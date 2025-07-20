34cbaef02209ab480352f97062943c15633f9af6
jd114506 <jonathon.bramich@my.jcu.edu.au>
2025-07-20 20:53:28 +1000

    Add project to Prac_07

--- prac_07/project.py
+++ prac_07/project.py
@@ -4,7 +4,7 @@ class Project:
     def __init__(self, name, start_date, priority, cost_estimate, completion_percentage):
         """Initialize project object"""
         self.name = name
-        self.start_date = datetime.strptime(start_date, '%d-%m-%Y').date()
+        self.start_date = datetime.strptime(start_date, '%d/%m/%Y').date()
         self.priority = priority
         self.cost_estimate = cost_estimate
         self.completion_percentage = completion_percentage
