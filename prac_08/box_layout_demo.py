685a355664a9ab5b397cc19dd3842556c248b45a
jd115406 <jonathon.bramich@my.jcu.edu.au>
2025-07-29 15:36:00 +1000

    Add detailed docstrings to box_layout_demo app

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -3,17 +3,19 @@ from kivy.lang import Builder
 
 
 class BoxLayoutDemo(App):
+    """BoxLayoutDemo app is an app that greets user input"""
     def build(self):
+        """Build GUI"""
         self.title = "Box Layout Demo"
         self.root = Builder.load_file('box_layout.kv')
         return self.root
 
     def handle_greet(self):
-        print("test")
-        self.root.ids.output_label.text = "Hello "
+        """Updates output label with greeting"""
         self.root.ids.output_label.text = f"Hello {self.root.ids.input_name.text}"
 
     def handle_clear(self):
+        """Clears label and input"""
         self.root.ids.output_label.text = ""
         self.root.ids.input_name.text = ""
 
