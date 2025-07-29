e82506861ddeeca49f89e48164beeb07e39a4477
jd115406 <jonathon.bramich@my.jcu.edu.au>
2025-07-29 15:32:03 +1000

    Add detailed docstrings for app and functions

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -9,12 +9,15 @@ CONVERSION = 1.60934
 
 
 class MilesConverterApp(App):
+    """MilesConverter App is an app that converts miles to kms"""
     def build(self):
+        """Build GUI"""
         self.title = "Convert miles to km"
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
 
     def get_valid_input(self):
+        """Get input from text_input and convert to float, otherwise set to 0.0"""
         try:
             value = float(self.root.ids.input_miles.text)
             return value
@@ -22,11 +25,13 @@ class MilesConverterApp(App):
             return 0.0
 
     def handle_calculate(self):
+        """Convert miles to km using conversion factor and update label"""
         value = self.get_valid_input()
         result = value * CONVERSION
         self.root.ids.output_label.text = str(result)
 
     def handle_increment(self, change):
+        """Increment input by desired value"""
         value = self.get_valid_input() + change
         self.root.ids.input_miles.text = str(value)
         self.handle_calculate()
