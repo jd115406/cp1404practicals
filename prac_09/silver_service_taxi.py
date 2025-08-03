a94b2b0a09f942003daa7acb844b0c39b925fccf
jd114506 <jonathon.bramich@my.jcu.edu.au>
2025-08-03 20:52:50 +1000

    Edit Class by adding funcstrings

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -5,15 +5,18 @@ CP1404 Practicals
 from prac_09.taxi import Taxi
 
 class SilverServiceTaxi(Taxi):
-    """"""
+    """Represents a Silver Service Taxi"""
     flagfall = 4.50
     def __init__(self, name, fuel, fanciness):
+        """Initialise the taxi"""
         super().__init__(name, fuel)
         self.fanciness = fanciness
         self.price_per_km *= fanciness
 
     def get_fare(self):
+        """Get the current fare"""
         return self.flagfall + super().get_fare()
 
     def __str__(self):
+        """Return a string representation of the taxi"""
         return f"{super().__str__()} plus flagfall of {self.flagfall:.2f}"
