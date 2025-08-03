20b955dce2cdc7afce0e3d247efa4fd9c15de821
jd114506 <jonathon.bramich@my.jcu.edu.au>
2025-08-03 20:48:03 +1000

    Add unreliable_car Class and unreliable_car_test program to Prac_09

---
+++ prac_09/unreliable_car.py
@@ -0,0 +1,19 @@
+"""
+CP1404 Practicals
+"""
+
+from prac_09.car import Car
+from random import randint
+
+class UnreliableCar(Car):
+    def __init__(self, name, fuel, reliability):
+        """Initialise a Taxi instance, based on parent class Car."""
+        super().__init__(name, fuel)
+        self.reliability = reliability
+
+    def drive(self, distance):
+        random_reliability = randint(1,100)
+        if random_reliability >= self.reliability:
+            distance = 0
+        distance_driven = super().drive(distance)
+        return distance_driven
