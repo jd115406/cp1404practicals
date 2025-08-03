9e330cda90c2b11002bf63d29191653fc64efd67
jd114506 <jonathon.bramich@my.jcu.edu.au>
2025-08-03 19:49:14 +1000

    Add taxi_test to Prac_09

---
+++ prac_09/taxi_test.py
@@ -0,0 +1,18 @@
+"""
+CP1404 Practical
+Test Taxi
+"""
+
+from prac_09.Taxi import Taxi
+
+def main():
+    """Test Taxi class"""
+    my_taxi = Taxi("Prius 1", 100, 1.23)
+    my_taxi.drive(40)
+    print(my_taxi)
+    my_taxi.start_fare()
+    my_taxi.drive(100)
+    print(my_taxi)
+
+main()
+
