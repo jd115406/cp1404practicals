5e0b5e4c771f23ac7a2dd360c65fd4ad73b770af
jd114506 <jonathon.bramich@my.jcu.edu.au>
2025-08-03 20:48:30 +1000

    Add silver_service_taxi Class and silver_service_taxi_test program to Prac_09

---
+++ prac_09/silver_service_taxi.py
@@ -0,0 +1,19 @@
+"""
+CP1404 Practicals
+"""
+
+from prac_09.taxi import Taxi
+
+class SilverServiceTaxi(Taxi):
+    """"""
+    flagfall = 4.50
+    def __init__(self, name, fuel, fanciness):
+        super().__init__(name, fuel)
+        self.fanciness = fanciness
+        self.price_per_km *= fanciness
+
+    def get_fare(self):
+        return self.flagfall + super().get_fare()
+
+    def __str__(self):
+        return f"{super().__str__()} plus flagfall of {self.flagfall:.2f}"
