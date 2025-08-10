068baba5bbd9422af909ea8296d00c9513f017bb
jd115406 <jonathon.bramich@my.jcu.edu.au>
2025-08-11 00:02:45 +1000

    Add wiki program to Prac_10

---
+++ prac_10/wiki.py
@@ -0,0 +1,18 @@
+"""
+CP1404 Practicals
+"""
+import wikipedia
+
+while True:
+    search_phrase = input("Enter a page title or search phrase (blank to quit): ")
+    if search_phrase == "":
+        break
+    try:
+        page = wikipedia.page(search_phrase, auto_suggest=False)
+        print(f"Title: {page.title}")
+        print(f"Summary: {page.summary}")
+        print(f"URL: {page.url}")
+    except wikipedia.exceptions.DisambiguationError as e:
+        print(f"Disambiguation Error: {e}")
+    except wikipedia.exceptions.PageError as e:
+        print(f"Page Error: {e}")
